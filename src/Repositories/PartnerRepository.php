01ed66479a3e1154284ecd82a5bda4d0beb3d6b4
Nurdaulet <artykbaev.nurdaulet@mail.ru>
2023-11-16 12:56:57 +0600

    ss

--- src/Repositories/PartnerRepository.php
+++ src/Repositories/PartnerRepository.php
@@ -9,11 +9,9 @@ class PartnerRepository
     public function get($cityId = null)
     {
         return config('flux-base.models.partner')::active()
-            ->select('user_id')
             ->with('partner:id,name,surname,company_name,avatar,avg_rating')
             ->when($cityId, fn($query) => $query->whereHas('cities', fn($query) => $query->where('city_id', $cityId)))
             ->limit(10)
-            ->groupBy('user_id')
             ->orderBy('lft')
             ->get();
     }
