29c58dee3144c6a37933eaa7925c622ed7ad8390
Nurdaulet <artykbaev.nurdaulet@mail.ru>
2023-12-20 11:32:58 +0600

    fix complaint

--- src/Repositories/ComplaintReasonRepository.php
+++ src/Repositories/ComplaintReasonRepository.php
@@ -2,15 +2,12 @@
 
 namespace Nurdaulet\FluxBase\Repositories;
 
-
-use App\Models\ComplaintReason;
-
 class ComplaintReasonRepository
 {
     public function get()
     {
         return  config('flux-base.models.complaint_reason')::active()
-            ->where('is_for_user', $request->is_for_user == 'true' ? 1 : 0)
+            ->where('is_for_user', request()->is_for_user == 'true' ? 1 : 0)
             ->orderBy('sort')->get();
     }
 }
