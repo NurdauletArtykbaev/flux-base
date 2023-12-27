fc709b9e33cd2435c8ea3838f2ce64ac74dbdf1e
Nurdaulet <artykbaev.nurdaulet@mail.ru>
2023-12-27 12:25:33 +0600

    fix partners

--- src/Filament/Resources/PartnerResource/Pages/ManagePartners.php
+++ src/Filament/Resources/PartnerResource/Pages/ManagePartners.php
@@ -17,35 +17,7 @@ class ManagePartners extends ManageRecords
     protected function getActions(): array
     {
         return [
-            Actions\CreateAction::make()      ->mutateFormDataUsing(function (array $data): array {
-                $url = env('AWS_URL') . '/' . $data['image'];
-//                dd($data['image']);
-                $imageExtension = explode('.', $data['image']);
-                $imageExtension = end($imageExtension);
-                $file = file_get_contents($url);
-
-                $image = ImageManagerStatic::make($file)->encode($imageExtension, 90);
-                $imageWebp = Image::make($file)->encode('webp', 80);
-                $webpName = 'partners/'.  time() . Str::uuid().'.webp';
-
-                $data['webp']  = $webpName;
-                Storage::disk('s3')->put($data['image'], $image);
-                Storage::disk('s3')->put($webpName, $imageWebp);
-
-                $url = env('AWS_URL') . '/' . $data['logo'];
-                $logoExtension = explode('.', $data['logo']);
-                $logoExtension = end($logoExtension);
-                $file = file_get_contents($url);
-                $image = ImageManagerStatic::make($file)
-                    ->encode($logoExtension, 90);
-                $imageWebp = Image::make($file)->encode('webp', 80);
-                $webpName = 'partners/'.  time() . Str::uuid().'.webp';
-
-                $data['logo_webp']  = $webpName;
-                Storage::disk('s3')->put($data['logo'], $image);
-                Storage::disk('s3')->put($webpName, $imageWebp);
-                return $data;
-            }),
+            Actions\CreateAction::make(),
         ];
     }
 }
