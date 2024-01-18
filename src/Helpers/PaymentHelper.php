3248dbafb9e9dfa319608aca33963330f0d80c9a
Nurdaulet <artykbaev.nurdaulet@mail.ru>
2024-01-18 17:53:30 +0600

    testing refund

--- src/Helpers/PaymentHelper.php
+++ src/Helpers/PaymentHelper.php
@@ -11,23 +11,28 @@ class PaymentHelper
     const STATUS_REFUND = 4;
     const STATUS_REFUND_FAILED = 'refund_failed';
 
+    const SLUG_CASH = 'cash';
+    const SLUG_CARD = 'card';
+    const SLUG_ALL = 'all';
+    const SLUG_DRAFT = 'draft';
+
 
     const ONLINE_EPAY           = 1;
     const ONLINE_CLOUD_PAYMENTS = 12;
 
 
-    public static function isPaymentMethodOnline($paymentMethodId): bool
+    public static function isPaymentOnline($slug): bool
     {
-        return in_array($paymentMethodId, [PaymentHelper::ONLINE_EPAY]);
+        return in_array($slug, [PaymentHelper::SLUG_CARD]);
     }
 
     public static function getSlugs()
     {
         return [
-            'draft' => trans('admin.payment_methods.draft'),
-            'all' => trans('admin.payment_methods.all'),
-            'card' => trans('admin.payment_methods.card'),
-            'cash' => trans('admin.payment_methods.cash'),
+            self::SLUG_DRAFT => trans('admin.payment_methods.'. self::SLUG_DRAFT),
+            self::SLUG_ALL => trans('admin.payment_methods.' . self::SLUG_ALL),
+            self::SLUG_CARD => trans('admin.payment_methods.' . self::SLUG_CARD),
+            self::SLUG_CASH => trans('admin.payment_methods.' . self::SLUG_CASH),
         ];
     }
 }
