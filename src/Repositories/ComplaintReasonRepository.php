4b866f72decb45a1becf91f0b9251330117d193a
Nurdaulet <artykbaev.nurdaulet@mail.ru>
2024-05-17 13:06:09 +0600

    test

--- src/Repositories/ComplaintReasonRepository.php
+++ src/Repositories/ComplaintReasonRepository.php
@@ -7,7 +7,7 @@ class ComplaintReasonRepository
     public function get($filters = [])
     {
         return  config('flux-base.models.complaint_reason')::active()
-            ->where('is_for_user', request()->is_for_user == 'true' ? 1 : 0)
+            ->where('is_for_user', request()->is_for_user == 'false' ? 0 : 1)
             ->when(!isset($filters['type']), fn($query) => $query->whereNull('type'))
             ->applyFilters(new ( config('flux-base.filters.complaint_reason')), $filters)
             ->orderBy('sort')->get();
